3a061e29fe5a7aee0049d6d7613d3dde93a2b3d1
ITI57060214121411182 <5706021411182@fitm.kmutnb.ac.th>
2016-02-11 02:13:05 +0700

    ห
    ห

--- one.java
+++ one.java
@@ -3,7 +3,7 @@ public class one {
 
 	public static void main(String[] args) {
 		System.out.print("Peerawich");
-        System.out.print("Peerawich");
+        	System.out.print("Peerawich");
         
 	}
 
