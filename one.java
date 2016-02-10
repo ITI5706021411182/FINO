eb70700f4e42a3ce5cb80e37fe6939b0c46cd9e9
ITI57060214121411182 <5706021411182@fitm.kmutnb.ac.th>
2016-02-11 02:12:23 +0700

    ส่งงานครั้งที่2

--- one.java
+++ one.java
@@ -2,8 +2,9 @@
 public class one {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		dd
+		System.out.print("Peerawich");
+        System.out.print("Peerawich");
+        
 	}
 
 }
